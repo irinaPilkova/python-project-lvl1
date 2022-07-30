28075f30d6981c8a64c943b918d6cd3dd9755de8
ipilkova@gmail.com <irinaPilkova>
2022-07-30 13:47:36 +0500

    cli.py

---
+++ brain_games/cli.py
@@ -0,0 +1,7 @@
+import prompt
+
+def welcome_user():
+        name = prompt.string('May I have your name?')
+        welcome = 'Hello,' + ' ' + name + '!'
+        print(welcome)
+        return welcome

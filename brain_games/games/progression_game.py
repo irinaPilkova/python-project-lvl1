9bf7993bfbc4db64a413ed639c13a8b8f3406b65
Irina Pilkova <ipilkova@gmail.com>
2022-08-29 18:12:12 +0300

    Progression

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -7,7 +7,7 @@ from random import randint
 def progres_question():
     """This function defines the question for the game."""
     num = randint(2, 6)
-    i_replace = randint(1, 10)
+    i_replace = randint(2, 9)
     progression = [(i * num) for i in range(1, 11)]
     correct_answer = progression[i_replace]
     progression[i_replace]= '..'
